20edabb6f1504329515691259227775db515df69
crasride <jose.dfernandez@yahoo.fr>
2022-12-01 16:28:36 +0100

    0. Gather data from an API vers.1.2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-""" module use urllib or requests api"""
-import json
-import urllib.request
+""" module use urllib or requests api """
+
+from requests import get
 from sys import argv
+import json
 
 
-# First we connect/open/read/loads to the data url users
-url = "https://jsonplaceholder.typicode.com/users/"
-Source_url = urllib.request.urlopen(url)
-data_url = Source_url.read()
-object = json.loads(data_url.decode('utf-8'))
+if __name__ == "__main__":
 
-# Now we get the name of the employee
-EMPLOYEE_NAME = object[int(argv[1]) - 1]["name"]
+    # First we connect/open/read/loads to the data url users
+    url_first = get('https://jsonplaceholder.typicode.com/todos/')
+    data_url_first = url_first.json()
+    completed = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = []
 
-# Second we connect/open/read/loads to the data url todos
-url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
-Source_url = urllib.request.urlopen(url)
-data_url = Source_url.read()
+    # Second we connect/open/read/loads to the data url todos
+    url_second = get('https://jsonplaceholder.typicode.com/users/')
+    data_url_second = url_second.json()
 
-# Now we get the name of the todo
-TOTAL_NUMBER_OF_TASKS = json.loads(data_url.decode('utf-8'))
-NUMBER_OF_DONE_TASKS = [i for i in TOTAL_NUMBER_OF_TASKS
-                        if i["completed"] is True]
+    for i in data_url_first:
+        if i.get('userId') == int(argv[1]):
+            TOTAL_NUMBER_OF_TASKS += 1
 
-# Format the name of the completed tasks
-print("Employee {} is done with tasks({}/{}):".format(
-    EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), len(TOTAL_NUMBER_OF_TASKS)
-))
-for NUMBER_OF_DONE_TASKS in NUMBER_OF_DONE_TASKS:
-    print("\t {}".format(NUMBER_OF_DONE_TASKS["title"]))
+            if i.get('completed') is True:
+                completed += 1
+                NUMBER_OF_DONE_TASKS.append(i.get('title'))
+
+    for i in data_url_second:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+
+    # Format the name of the completed tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee, completed, TOTAL_NUMBER_OF_TASKS))
+
+    for NUMBER_OF_DONE_TASKS in NUMBER_OF_DONE_TASKS:
+        print("\t {}".format(NUMBER_OF_DONE_TASKS))
